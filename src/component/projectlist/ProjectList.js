ac05607cefee90c7ec32a203c89400e871c78eea
Anoruo Benjamin <82318783+Benjamin4041@users.noreply.github.com>
2024-11-23 23:56:51 +0000

    made cnahges

---
+++ src/component/projectlist/ProjectList.js
@@ -0,0 +1,75 @@
+import React from "react";
+import "./ProjectList.css";
+
+export default function ProjectList() {
+  return (
+    <>
+      <div className="brandingMainCon_section2-innerCon3">
+        <span className="brandingMainCon_section2-clubhouse">
+          {/* <div className="viewcaseCon">
+            <img src="/images/Rectangle.svg" alt="" />
+            <div className="bigcircle">
+              <div className="smallcircle">
+                <p className="smallcircle_p">View case</p>
+              </div>
+            </div>
+          </div> */}
+          <span className="textandTextFilter">
+            {/* <div className='textandTextFilte_textFilter'>
+
+                        </div> */}
+            <p className="textandTextFilte_Text">
+              clubhouse <br />
+              <span className="flex textandTextFilte_Text-innerText">
+                /branding <img src="/images/star2.svg" alt="" /> aug.2021
+              </span>
+            </p>
+          </span>
+        </span>
+        <span className="brandingMainCon_section2-clubhouse">
+          {/* <div className="viewcaseCon">
+            <img src="/images/Rectangle.svg" alt="" />
+            <div className="bigcircle">
+              <div className="smallcircle">
+                <p className="smallcircle_p">View case</p>
+              </div>
+            </div>
+          </div> */}
+          <span className="textandTextFilter">
+            {/* <div className='textandTextFilte_textFilter'>
+
+                        </div> */}
+            <p className="textandTextFilte_Text">
+              raven bank
+              <br />
+              <span className="flex textandTextFilte_Text-innerText">
+                /branding <img src="/images/star2.svg" alt="" /> aug.2021
+              </span>
+            </p>
+          </span>
+        </span>
+        <span className="brandingMainCon_section2-clubhouse">
+          {/* <div className="viewcaseCon">
+            <img src="/images/Rectangle.svg" alt="" />
+            <div className="bigcircle">
+              <div className="smallcircle">
+                <p className="smallcircle_p">View case</p>
+              </div>
+            </div>
+          </div> */}
+          <span className="textandTextFilter">
+            {/* <div className='textandTextFilte_textFilter'>
+
+                        </div> */}
+            <p className="textandTextFilte_Text">
+              yellostar <br />
+              <span className="flex textandTextFilte_Text-innerText">
+                /branding <img src="/images/star2.svg" alt="" /> aug.2021
+              </span>
+            </p>
+          </span>
+        </span>
+      </div>
+    </>
+  );
+}
